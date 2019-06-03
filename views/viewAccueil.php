54335a0fb7422415f574efd059b80290a1ad88e2
owalid <othmane.el.ayadi@gmail.com>
2019-06-03 02:52:38 +0200

    fix for long comment

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -32,84 +32,87 @@
         <section class="columns">
             <div class="column"></div>
             <div class="column">
-                <article class="card is-rounded">
-                    <div class="card-content">
-                        <div class="field">
-                            <div class="">
-                                <div class="hovereffect padding-20-bottom">
-                                    <div class="columns is-gapless">
+            <div class="box">
+                <article class="media">
+                    <div class="media-left">
+                    <figure class="image is-64x64">
+                        <img class="is-rounded" src="<?=$usr->getPp()?>">
+                    </figure>
+                    </div>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                            <strong><?= $usr->getLogin() ?></strong>
+                            <br>                          
+                            </p>
+                            <figure class="image">
+                                <img ondblclick="like(<?=$img->getIdImg()?>)" src="<?=$img->getImg()?>">
+                            </figure>
+                        </div>
+                        <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="button is-rounded" href="<?=URL?>?url=image&like=yes&idImg=<?=$img->getIdimg()?>">
+                                    <span class="icon has-text-danger is-small">
+                                        <i class="fas fa-heart"></i>
+                                    </span>
+                                </a>
+                                <a class="button is-rounded" href="#comment<?=$img->getIdImg()?>">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-comment-alt"></i>
+                                    </span>
+                                </a>
+                                <a class="button is-rounded" href="<?=URL?>?url=image&save=yes&idImg=<?=$img->getIdImg()?>">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-bookmark"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        </nav>
+                        <p class="has-text-weight-semibold">Aimé par <?=$img->getNbLike()?> personnes</p>
+                        <p><span class="has-text-weight-semibold"><?= $usr->getLogin() ?></span>: <?= $img->getDescription()?></p>
+                        <hr />
+                        <?php
+                        if ($comment)
+                        {
+                            foreach($comment as $com)
+                            {
+                                ?>
+                                <hr />
+                                <br />
+                                <div class="content">
+                                    
+                                <div class="media-left ">
+                                    <div class="columns ">
                                         <div class="column is-vcentered">
                                             <figure class="image is-64x64">
-                                                <img class="is-rounded" src="<?=$usr->getPp()?>">
+                                                <img class="is-rounded" src="<?= $com['pp']?>">
                                             </figure>
+
                                         </div>
-                                        <div class="column is-7 is-vcentered">
-                                            <p><?= $usr->getLogin() ?></p>
-                                        </div>
-                                            </div>
+                                        <div class="column is-11 is-vcentered">
+                                            
+                                            <strong><?= $com['login']?>:</strong>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="padding-20-bottom">
-                                    <div class="columns">
-                                        <div class="column">
-                                            <figure class="image">
-                                                <img ondblclick="like(<?=$img->getIdImg()?>)" src="<?=$img->getImg()?>">
-                                            </figure>
-                                                <div class="buttons is-centered is-vcentered padding-10-top">
-                                                    <a class="button is-rounded" href="<?=URL?>?url=image&like=yes&idImg=<?=$img->getIdimg()?>">
-                                                    <span class="icon has-text-danger is-small">
-                                                        <i class="fas fa-heart"></i>
-                                                    </span>
-                                                    </a>
-                                                    <a class="button is-rounded" href="#comment<?=$img->getIdImg()?>">
-                                                    <span class="icon is-small">
-                                                        <i class="fas fa-comment-alt"></i>
-                                                    </span>
-                                                    </a>
-                                                    <a class="button is-rounded" href="<?=URL?>?url=image&save=yes&idImg=<?=$img->getIdImg()?>">
-                                                    <span class="icon is-small">
-                                                        <i class="fas fa-bookmark"></i>
-                                                    </span>
-                                                    </a>
-                                               </div>
-                                                    <p class="has-text-weight-semibold">Aimé par <?=$img->getNbLike()?> personnes</p>
-                                            <p><span class="has-text-weight-semibold"><?= $usr->getLogin() ?></span>: <?= $img->getDescription()?></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div class="hero-foot">
-                                        <?php
-                                        if ($comment)
-                                        {
-                                            foreach($comment as $com)
-                                            {
-                                                ?>
-                                                <div class="columns is-gapless is-vcentered">
-                                                <div class="column is-vcentered">
-                                                    <figure class="image is-32x32">
-                                                        <img class="is-rounded" src="<?= $com['pp']?>">
-                                                    </figure>
-                                                </div>
-                                        <div class="column is-10 is-vcentered">
-                                            <span class="has-text-weight-semibold"><?= $com['login']?>: </span><?=  $com['commentaire'] ?></p>
-                                        </div>
-                                        <br />
-                                        </div>
-                                        <?php 
-                                         }
-                                        }
-                                        else
-                                        {
-                                            ?>
-                                        <div class="column is-11 is-vcentered">
-                                            <span class="has-text-weight-semibold">Pas de commentaire 🤷‍♂️</p>
-                                        </div>
-                                        <?php
-                                        }
-                                            ?>
-                                        <hr />
+                                <p>
+                                <br>
+                                <?=  $com['commentaire'] ?>
+                                </p>
+                        </div>
+                        <?php 
+                         }
+                        }
+                        else
+                        {
+                            ?>
+                        <div class="column is-11 is-vcentered">
+                            <span class="has-text-weight-semibold">Pas de commentaire 🤷‍♂️</p>
+                        </div>
+                        <?php
+                        }
+                            ?>
+                               <hr />
                                         <form method="post" action="<?=URL?>?url=image&comment=yes&idImg=<?=$img->getIdImg()?>">
                                             <div class="field has-addons">
                                                 <div class="control is-expanded">
@@ -120,10 +123,10 @@
                                                 </div>
                                             </div>
                                         </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>
+                    </div>
+                </article>
+                </div>
+    
                     </div>
                     <div class="column"></div>
                 </section>
